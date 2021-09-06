a5ec9fb9317211f3b05a333dbc1d928c34563204
Kevin Yeh <kevin@shopvida.com>
2021-09-06 13:32:51 -0700

    set up firebase and request fetch call for data; displayed sidebar nav list

--- App.js
+++ App.js
@@ -7,14 +7,23 @@ import './global.scss'
 
 const App = () => {
 	const [appTheme, setAppTheme] = useState('Dark')
+	const [sidebarState, setSidebarState] = useState('open')
+
+	const toggleSidebarHandler = sidebarState => {
+		if (sidebarState === 'open') {
+			setSidebarState('close')
+		} else {
+			setSidebarState('open')
+		}
+	}
 
 	const changeThemeHandler = theme => {
 		setAppTheme(theme)
 	}
 	
   return (
-    <main className="app" data-app-theme={appTheme}>
-			<Sidebar />
+    <main className="app" data-app-theme={appTheme} data-sidebar-toggle={sidebarState}>
+			<Sidebar sidebarState={sidebarState} toggleSidebar={(state) => toggleSidebarHandler(state)} />
 			<div className="app__body">
 				<Navigation appTheme={appTheme} changeTheme={(theme) => changeThemeHandler(theme)} />
 				<Home />
