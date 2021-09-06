624c097d6375bfcf255317f3b12e01930172498e
Kevin Yeh <kevin@shopvida.com>
2021-09-06 13:32:51 -0700

    set up firebase and request fetch call for data; displayed sidebar nav list

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,21 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Sidebar.scss'
 
-const Sidebar = () => {
-	const [sidebarState, setSidebarState] = useState('open');
+const Sidebar = (props) => {
+	const [navList, setNavList] = useState([])
 
-	const toggleSidebarHandler = sidebarState => {
-		if (sidebarState === 'open') {
-			setSidebarState('close')
-		} else {
-			setSidebarState('open')
+	useEffect(() => {
+		fetch('https://portfolio-v2-2237f-default-rtdb.firebaseio.com/navigation.json'
+		).then((response) => {
+			return response.json()
+		}).then((data) => {
+			console.log('data', data)
+			setNavList(data)
+		})
+	}, [])
+
+	const displaySidebarList = listArr => {
+		let listHtml = []
+		for (let i = 0; i < listArr.length; i++) {
+			listHtml.push(
+				<li key={i}>
+					{/* <a href="">{listArr[i].title}</a> */}
+					{listArr[i].title}
+					{listArr[i].sublinks && listArr[i].sublinks.length > 0 ?
+					<ul className="sidebar__sublist">{displaySidebarList(listArr[i].sublinks)}</ul> : ''}
+					<span className="sidebar__list--dropdown" onClick={() => {}}></span>
+				</li>
+			)
 		}
+		return listHtml
 	}
 
 	return (
-		<section className="sidebar" data-panel-toggle={sidebarState}>
-			<button className="sidebar__collapse" onClick={() => toggleSidebarHandler(sidebarState)}>&#x2039;</button>
-			<ul className="sidebar__list"></ul>
+		<section className="sidebar" data-sidebar-toggle={props.sidebarState}>
+			<button className="sidebar__collapse" onClick={() => props.toggleSidebar(props.sidebarState)}>&#x2039;</button>
+			<ul className="sidebar__list">{displaySidebarList(navList)}</ul>
 		</section>
 	)
 }
