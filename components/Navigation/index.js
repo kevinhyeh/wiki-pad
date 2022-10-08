2193d17ca41fdfc22adfdc793077ebd8ea489334
Kevin Yeh <kevin@shopvida.com>
2022-10-08 08:15:14 -0700

    added pusling dot animation

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,33 +1,23 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import { IconArrowLine } from '../elements/Icons';
 import './Navigation.scss';
 
 const Navigation = (props) => {
-	const [themeToggleState, setThemeToggleState] = useState('close')
-	const themeSelectRef = useRef()
-
-	const toggleSelectHandler = themeToggleState => {
-		if (themeToggleState === 'open') {
-			setThemeToggleState('close')
-		} else {
-			setThemeToggleState('open')
-			themeSelectRef.current.focus()
-		}
+	const handleNavigationClick = (title) => {
+		let pathName = title === 'Home' ? '' : title.toLowerCase()
+		window.history.pushState({},"", pathName);
+		props.handlePortfolioClick(title, 'breadcrumb')
 	}
-
 	return (
 		<section className="navigation">
-			<div className="navigation__select" data-select-toggle={themeToggleState}>
-				<button className="navigation__select--toggle" ref={themeSelectRef} onClick={() => toggleSelectHandler(themeToggleState)} onBlur={() => toggleSelectHandler(themeToggleState)}>
-					<span>{props.appTheme}</span>
-					<IconArrowLine direction="down" />
-				</button>
-				<ul className="navigation__select--list">
-					<button className="navigation__select--option first" onClick={() => props.changeTheme('Dark')}>Dark</button>
-					<button className="navigation__select--option last" onClick={() => props.changeTheme('Light')}>Light</button>
-					{/* <li className="navigation__select--option last" onClick={() => props.changeTheme('Kevin')}>Kevin</li> */}
-				</ul>
-			</div>
+			{props.breadcrumb ? props.breadcrumb.map((item, index) => {
+				return(
+					<React.Fragment key={index}>
+						<span onClick={() => handleNavigationClick(item)} className="navigation__link text-xs">{item}</span>
+						{index + 1 < props.breadcrumb.length ? <IconArrowLine direction="right" /> : ''}
+					</React.Fragment>
+				)
+			}) : ''}
 		</section>
 	)
 }
