75615f12f58c922a6f9e01f4a61ecbb204e44b50
Kevin Yeh <kevin@shopvida.com>
2022-10-14 22:46:39 -0700

    finished about; started timeline;

--- components/timeline/index.js
+++ components/timeline/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './Timeline.scss';
+import { HighSchool, College, Bootcamp } from '../elements/Icons'
+
+const educationIcons = {
+	HighSchool,
+	College,
+	Bootcamp
+}
 
 const Timeline = (props) => {
-
+	
 	return (
-		<div className="timeline">
-			
+		<div className="timeline animate">
+			{props.data ? props.data.map((obj, index) => {
+				let MyComponent = educationIcons[obj.title.replace(' ', '')]
+				return (
+					<React.Fragment key={index}>
+						<div className="timeline__icon" data-index={index + 1}>
+							<MyComponent />
+						</div>
+						<div className="timeline__info" data-index={index + 1}>
+							<p className="text-base">{obj.info}</p>
+						</div>
+					</React.Fragment>
+				)
+			})
+			: ''}
+			<div className="timeline__line"></div>
 		</div>
 	)
 }
